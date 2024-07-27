ebbd6f293e9ff4b3b301b7237046219d8803e6b6
Kaito Udagawa <umireon@gmail.com>
2024-07-27 20:03:08 +0900

    Add basic structure

---
+++ src/AudioStreamingFilterContext.cpp
@@ -0,0 +1,51 @@
+#include "AudioStreamingFilterContext.h"
+
+#include "plugin-support.h"
+
+#include <obs-frontend-api.h>
+
+static void handleFrontendEventCallback(obs_frontend_event event, void *private_data)
+{
+  AudioStreamingFilterContext *self = reinterpret_cast<AudioStreamingFilterContext*>(private_data);
+  self->handleFrontendEvent(event);
+}
+
+AudioStreamingFilterContext::AudioStreamingFilterContext(obs_data_t *settings, obs_source_t *source) :
+  settings(settings),
+  source(source)
+{
+  obs_frontend_add_event_callback(handleFrontendEventCallback, this);
+}
+
+AudioStreamingFilterContext::~AudioStreamingFilterContext(void) {
+  obs_frontend_remove_event_callback(handleFrontendEventCallback, this);
+}
+
+
+obs_properties_t *AudioStreamingFilterContext::getProperties(void) {
+  obs_properties_t *props = obs_properties_create();
+  return props;
+}
+
+obs_source_frame *AudioStreamingFilterContext::filterVideo(struct obs_source_frame *frame) {
+  obs_log(LOG_INFO, "Filter Video: %llu", frame->timestamp);
+  return frame;
+}
+
+obs_audio_data *AudioStreamingFilterContext::filterAudio(struct obs_audio_data *audio) {
+  obs_log(LOG_INFO, "Filter Audio: %llu", audio->timestamp);
+  return audio;
+}
+
+void AudioStreamingFilterContext::handleFrontendEvent(obs_frontend_event event)
+{
+  if (event == OBS_FRONTEND_EVENT_STREAMING_STARTING) {
+    obs_log(LOG_INFO, "Streaming starting!");
+  } else if (event == OBS_FRONTEND_EVENT_STREAMING_STOPPING) {
+    obs_log(LOG_INFO, "Streaming stopping!");
+  } else if (event == OBS_FRONTEND_EVENT_RECORDING_STARTING) {
+    obs_log(LOG_INFO, "Recording starting!");
+  } else if (event == OBS_FRONTEND_EVENT_RECORDING_STOPPING) {
+    obs_log(LOG_INFO, "Recording stopping!");
+  }
+}
---
+++ src/AudioStreamingFilterContext.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <obs.h>
+#include <obs-frontend-api.h>
+
+class AudioStreamingFilterContext {
+  const obs_data_t *settings;
+  const obs_source_t *source;
+
+  video_t *video;
+  obs_encoder_t *videoEncoder;
+
+  audio_t *audio;
+  obs_encoder_t *audioEncoder;
+  obs_output_t *fileOutput;
+
+public:
+  AudioStreamingFilterContext(obs_data_t *settings, obs_source_t *source);
+  ~AudioStreamingFilterContext(void);
+  obs_properties_t *getProperties(void);
+  obs_source_frame *filterVideo(obs_source_frame *frame);
+  obs_audio_data *filterAudio(obs_audio_data *audio);
+  void handleFrontendEvent(obs_frontend_event event);
+};
