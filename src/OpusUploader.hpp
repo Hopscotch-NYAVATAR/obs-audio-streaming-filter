174f5165e80867821dd3e4debd0505d8c7f929b7
Kaito Udagawa <umireon@gmail.com>
2024-08-16 04:56:52 +0900

    Fix

--- src/OpusUploader.hpp
+++ src/OpusUploader.hpp
@@ -7,7 +7,12 @@
 
 #include <opusenc.h>
 
-#include "RecordPathGenerator.hpp"
+#include "AudioRecordClient.hpp"
+
+struct SegmentEntry {
+	const int segmentIndex;
+	const std::filesystem::path segmentPath;
+};
 
 class OpusUploader {
 	const std::filesystem::path outputDirectory;
@@ -18,6 +23,14 @@ class OpusUploader {
 	OggOpusEnc *encoder;
 
 	int segmentIndex;
+	std::vector<SegmentEntry> segmentEntries;
+
+	int destinationStart;
+	int destinationCount;
+	std::vector<std::string> destinationURLs;
+
+	AudioRecordClient &audioRecordClient;
+	AuthClient &authClient;
 
 	static std::filesystem::path
 	generateNextStreamPath(const std::filesystem::path &outputDirectory,
@@ -35,12 +48,18 @@ class OpusUploader {
 	}
 
 public:
+	int destinationBatchCount = 200;
+	int destinationBatchBackoff = 100;
+
 	OpusUploader(std::filesystem::path _outputDirectory,
 		     std::string _outputExt, std::string _outputPrefix,
-		     int sampleRate)
+		     int sampleRate, AudioRecordClient &_audioRecordClient,
+		     AuthClient &_authClient)
 		: outputDirectory(_outputDirectory),
 		  outputExt(_outputExt),
-		  outputPrefix(_outputPrefix)
+		  outputPrefix(_outputPrefix),
+		  audioRecordClient(_audioRecordClient),
+		  authClient(_authClient)
 	{
 		using std::filesystem::path;
 
@@ -49,7 +68,7 @@ public:
 		path outputPath(generateNextStreamPath(outputDirectory,
 						       outputExt, outputPrefix,
 						       segmentIndex));
-
+		segmentEntries.push_back({segmentIndex, outputPath});
 		std::filesystem::create_directories(outputPath.parent_path());
 
 		int error;
@@ -77,6 +96,7 @@ public:
 		path outputPath(generateNextStreamPath(outputDirectory,
 						       outputExt, outputPrefix,
 						       segmentIndex));
+		segmentEntries.push_back({segmentIndex, outputPath});
 		std::filesystem::create_directories(outputPath.parent_path());
 
 		int error = ope_encoder_continue_new_file(
@@ -85,6 +105,54 @@ public:
 			obs_log(LOG_ERROR, "%s", ope_strerror(error));
 			return false;
 		}
+		return true;
+	}
+
+	bool uploadPendingSegments(void)
+	{
+		if (destinationURLs.empty()) {
+			std::string idToken = authClient.getIdToken();
+			const auto destinationResult =
+				audioRecordClient.getDestinations(
+					outputExt, outputPrefix, 0,
+					destinationBatchCount, idToken);
+			destinationStart = destinationResult.start;
+			destinationCount = destinationResult.count;
+			destinationURLs = destinationResult.destinations;
+		}
+
+		for (const SegmentEntry &segmentEntry : segmentEntries) {
+			const int &index = segmentEntry.segmentIndex;
+			const int &start = destinationStart;
+			const int &count = destinationCount;
+			const int &batch = destinationBatchCount;
+			const int &backoff = destinationBatchBackoff;
+
+			if (index < start || index >= start + count) {
+				obs_log(LOG_ERROR, "Invalid segment sequence!");
+				return false;
+			}
+
+			if (index + backoff <= start + count) {
+				std::string idToken = authClient.getIdToken();
+				const auto destinationResult =
+					audioRecordClient.getDestinations(
+						outputExt, outputPrefix,
+						start + count, batch, idToken);
+				const auto &newURLs =
+					destinationResult.destinations;
+				destinationCount += destinationResult.count;
+				destinationURLs.insert(destinationURLs.end(),
+						       newURLs.begin(),
+						       newURLs.end());
+			}
+
+      const auto &segmentPath = segmentEntry.segmentPath;
+      const auto &uploadURL = destinationURLs[segmentEntry.segmentIndex - destinationStart];
+			obs_log(LOG_INFO, "Uploading %s to %s",
+				segmentPath.string().c_str(), uploadURL.c_str());
+		}
+
 		return true;
 	}
 
